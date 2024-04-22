a1c30e13a149beb0c3f5814296068ea9a04f0c34
zoolotoui <chag.med.amine@gmail.com>
2024-04-22 12:04:14 +0100

    core:working on consultant profile

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -30,7 +30,12 @@
                 Inactive Link = hover:text-indigo-500 -->
                         <li class="md:px-4 md:py-2 text-white"><a href="/">Home</a></li>
                         <li class="md:px-4 md:py-2 text-white"><a href="/about">About</a></li>
+                        @if(Auth::user()->role == 'consultant' )
+                        <li class="md:px-4 md:py-2 text-white"><a href="#">Mon espace</a></li>
+                        @else
                         <li class="md:px-4 md:py-2 text-white"><a href="/explore">Explore</a></li>
+                        @endif
+                        
                         <li class="md:px-4 md:py-2 text-white"><a href="#">Blog</a></li>
                         
                     </ul>
